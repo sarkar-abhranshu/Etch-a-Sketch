613a58c359290ed56e9c7e90697d859738b792bd
sarkar-abhranshu <abhranshusarkar@outlook.com>
2024-11-01 19:21:39 +0530

    Modify code to include change grid button, size input from user changes divs in the grid

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
+GRIDSIZE = 960;
+
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.container');
+    const button = document.querySelector('.gridSize');
+    
+    button.addEventListener('click', () => {
+        let size = prompt("Enter a new grid size: ");
+        if(size <= 0 || size > 100) {
+            alert("Invalid :( Please input a number between 1 and 100");
+            return;
+        }
+        grid(size);
+    });
 
-    for(let i = 0; i < 16; i++){
-        for(let j = 0; j < 16; j++){
-            const div = document.createElement('div');
-            container.appendChild(div);
+    function grid(size = 16) {
+        container.innerHTML = '';
+        let cellSize = GRIDSIZE / size + 'px';
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                const div = document.createElement('div');
+                div.style.flex = `0 0 calc(100% / ${size})`;
+                div.style.height = cellSize;
+                container.appendChild(div);
+            }
         }
     }
-    const cell = document.querySelectorAll('div');
-    for(let i=1; i<=256; i++){
-        cell[i].addEventListener('mouseover', () => {
-            cell[i].style.backgroundColor = 'green';
-        });
-    };
+
+    container.addEventListener('mouseover', (event) => {
+        if (event.target !== container) {
+            event.target.style.backgroundColor = 'green';
+        }
+    });
+
+    grid();
 });
